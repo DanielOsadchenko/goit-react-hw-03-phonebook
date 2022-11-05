f87e999a180fd03fbf59d30c96b08694cfa56053
DanielOsadchenko <dm.osadchenko@gmail.com>
2022-11-05 16:16:40 +0100

    Local Storage saving completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,30 @@ import ContactForm from './ContactForm/ContactForm';
 import { Contacts } from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 
+const LS_KEY = 'saved_contacts';
+
 export default class App extends Component {
   state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  contacts: [],
   filter: '',
+  }
+
+  componentDidMount() {
+    let parsedContacts = [];
+
+    if (localStorage.getItem(LS_KEY)) {
+      parsedContacts = JSON.parse(localStorage.getItem(LS_KEY));
+    }
+    if (parsedContacts.length !== 0) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state) {
+      const prepareContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem(LS_KEY, prepareContacts);
+    }
   }
 
   addContact = (newContact) => {
